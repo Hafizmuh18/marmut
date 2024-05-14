91d6b4d4515e2b851cc1d02ff0fd2c6935c199c4
Hafizmuh18 <hafizmuh18@gmail.com>
2024-05-14 17:49:18 +0700

    save state

--- authentikasi/views.py
+++ authentikasi/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
+from django.db import connection
 
 @csrf_exempt
 def login(request):
+    with connection.cursor() as cursor:
+        cursor.execute("select * from akun")
+        data = cursor.fetchall()
+        print(data)
     return render(request, 'auth_login.html')
--- marmut/settings.py
+++ marmut/settings.py
@@ -139,10 +139,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
